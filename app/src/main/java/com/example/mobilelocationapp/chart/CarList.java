4d6579911802d8494baf9add1aa6c8d031f03938
hejun <fengz990702@gmail.com>
2022-04-08 17:06:56 +0800

    v2.7 2022_4_8

---
+++ app/src/main/java/com/example/mobilelocationapp/chart/CarList.java
@@ -0,0 +1,63 @@
+package com.example.mobilelocationapp.chart;
+
+import java.util.ArrayList;
+
+public class CarList {
+    private ArrayList<RealPoint> realPointList = new ArrayList<>();
+    private ArrayList<TargetPoint> targetPointList = new ArrayList<>();
+
+    private int port;
+
+    public CarList(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public ArrayList<RealPoint> getRealPointList() {
+        return realPointList;
+    }
+
+    public ArrayList<TargetPoint> getTargetPointList() {
+        return targetPointList;
+    }
+
+
+    public void addRealPoint(double x, double y){
+        addRealPoint(new RealPoint(x, y));
+    }
+
+    public void addRealPoint(RealPoint realPoint){
+        realPointList.add(realPoint);
+    }
+
+
+    public void addTargetPoint(double x, double y){
+        addTargetPoint(new TargetPoint(x, y));
+    }
+
+    public void addTargetPoint(TargetPoint targetPoint){
+        targetPointList.add(targetPoint);
+    }
+
+    public ArrayList<Double> getXError(){
+        ArrayList<Double> xErrorList = new ArrayList<>();
+        int min = Math.min(realPointList.size(), targetPointList.size());
+        for (int i = 0; i < min; i++) {
+            xErrorList.add(realPointList.get(i).getX() - targetPointList.get(i).getX());
+        }
+        return xErrorList;
+    }
+
+    public ArrayList<Double> getYError(){
+        ArrayList<Double> yErrorList = new ArrayList<>();
+        int min = Math.min(realPointList.size(), targetPointList.size());
+        for (int i = 0; i < min; i++) {
+            yErrorList.add(realPointList.get(i).getY() - targetPointList.get(i).getY());
+        }
+        return yErrorList;
+    }
+
+}
